60917d5142c30307256ecc9a5a7a5ecea6794456
Denis <denis.klimov@corp.mail.ru>
2021-12-09 17:44:16 +0300

    Сдаю пустую дз что бы не пропустить дедлайн, доделаю

---
+++ src/main/java/HomeWork3/HomeWork.java
@@ -0,0 +1,7 @@
+package HomeWork3;
+
+public class HomeWork {
+    public static void main(String[] args) {
+            System.out.println("Не успел сделать дз, сдаю пустую что бы не просрочить");
+        }
+}
